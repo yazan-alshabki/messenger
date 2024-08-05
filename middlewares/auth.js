33e29e83b0b42ce8527027f760beabd3b7fc3b43
yazan <yazanalshabki2001@gmail.com>
2024-08-05 17:25:00 +0300

    session

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,31 +1,29 @@
 const isLogin = async (req, res, next) => {
   const token = req.cookies.user;
-  try {
-    if (token) {
-      next();
-    } else {
-      res.redirect("/");
-    }
-  } catch (err) {
-    console.log(err.message);
-  }
   // try {
-  //   if (!req.session.user) {
+  //   if (token) {
+  //     next();
+  //   } else {
   //     res.redirect("/");
   //   }
-  //   next();
   // } catch (err) {
   //   console.log(err.message);
   // }
+  try {
+    if (!req.session.user) {
+      res.redirect("/");
+    }
+    next();
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 const isLogout = async (req, res, next) => {
   try {
-    const token = req.cookies.user;
-    if (token) {
+    if (req.session.user) {
       res.redirect("/dashboard");
     }
     next();
-    
   } catch (err) {
     console.log(err.message);
   }
